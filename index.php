1d6318a6a3316cd2188124e78f39232b7680132b
Nouhaila731 <153520821+Nouhaila731@users.noreply.github.com>
2024-01-12 20:46:44 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,503 @@
+<html>
+<head>
+<title>chez le delice</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  	
+    
+
+
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="style.css" />
+
+
+
+ 
+
+    <style>
+       .wrapper {  
+  min-height: 100vh;  
+  color: #000;  
+  font-family: 'Muli', sans-serif;  
+  font-size: 1rem;  
+  background-color: #F1E8E6;  
+  display: -ms-grid;  
+  display: grid;  
+  place-items: center;  
+ }  
+ .container {  
+  background-color: #fff;  
+  padding: 1em;  
+  border-radius: 1em;  
+  max-width: 768px;  
+  display: -ms-grid;  
+  display: grid;  
+  place-items: center;  
+  -webkit-box-shadow: 0px 17px 34px -20px #f55951;  
+      box-shadow: 0px 17px 34px -20px #f55951;  
+ }  
+ .title {  
+  font-size: 1.6rem;  
+  font-weight: 700;  
+ }  
+ .text {  
+  font-size: 1rem;  
+  margin-bottom: 0.8em;  
+ }  
+ .container {  
+  max-width: 400px;  
+  overflow: hidden;  
+  padding: 0;  
+ }  
+ .img {  
+  width: 100%;  
+  height: auto;  
+  border-radius: 1em 1em 0 0;  
+  -o-object-fit: cover;  
+    object-fit: cover;  
+  -webkit-transform: translateY(-50%);  
+      transform: translateY(-50%);  
+ }  
+ .img__container {  
+  height: 200px;  
+  overflow: hidden;  
+  margin-bottom: 1.5em;  
+ }  
+ .title {  
+  margin-bottom: 0.9em;  
+ }  
+ .subtitle {  
+  font-size: 0.9rem;  
+  text-transform: uppercase;  
+  margin-bottom: 0.5em;  
+  font-weight: 700;  
+ }  
+ .content {  
+  display: -ms-grid;  
+  display: grid;  
+  place-items: center;  
+  padding: 0 2em;  
+  margin-bottom: 1.8em;  
+ }  
+ .mail {  
+  padding: 0.5em 0 0.5em 1.5em;  
+  line-height: 3;  
+  width: 100%;  
+  border: 1px solid #d9d9d9;  
+  border-radius: 0.5em;  
+  margin-bottom: 1em;  
+ }  
+ ::-webkit-input-placeholder {  
+  color: #111127;  
+ }  
+ :-ms-input-placeholder {  
+  color: #111127;  
+ }  
+ ::-ms-input-placeholder {  
+  color: #111127;  
+ }  
+ ::placeholder {  
+  color: #111127;  
+ }  
+ .subscribe {  
+  color: #fff;  
+  font-size: 1.3rem;  
+  font-weight: 700;  
+  background-color: #f55951;  
+  padding: 0.9em 0;  
+  display: inline-block;  
+  border: none;  
+  border-radius: 0.5em;  
+  width: 100%;  
+  margin-bottom: 1.3em;  
+ }  
+ .text {  
+  font-size: 0.9rem;  
+  width: 90%;  
+  text-align: center;  
+ }  
+
+
+
+
+
+
+body, h1,h2,h3,h4,h5,h6 {font-family: Verdana, Geneva, Tahoma, sans-serif;}
+.w3-row-padding img {margin-bottom: 12px}
+/* Set the width of the sidebar to 120px */
+.w3-sidebar {width: 120px;background: #626060;}
+/* Add a left margin to the "page content" that matches the width of the sidebar (120px) */
+#main {margin-left: 120px}
+/* Remove margins from "page content" on small screens */
+@media only screen and (max-width: 600px) {#main {margin-left: 0}}
+</style>
+</head>
+<body>
+
+  <div class="a w3-black nouha-hide-small;">
+    <a href="#" class="w3-bar-item w3-button"><i class="fa fa-facebook-official"></i></a>
+    <a href="#" class="w3-bar-item w3-button"><i class="fa fa-instagram"></i></a>
+    <a href="#" class="w3-bar-item w3-button"><i class="fa fa-snapchat"></i></a>
+    <a href="#" class="w3-bar-item w3-button"><i class="fa fa-flickr"></i></a>
+    <a href="#" class="w3-bar-item w3-button"><i class="fa fa-twitter"></i></a>
+    <a href="#" class="w3-bar-item w3-button"><i class="fa fa-linkedin"></i></a>
+    <a href="#" class="w3-bar-item w3-button w3-right"><i class="fa fa-search"></i></a>
+  </div>
+<!-- Icon Bar (Sidebar - hidden on small screens) -->
+<nav class="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center">
+
+
+
+
+  <!-- Avatar image in top left corner -->
+  <img src="Design Gallery _ Designhill.jpeg" style="width:100%">
+  <a href="#" class="w3-bar-item w3-button w3-padding-large w3-gris">
+    <i class="fa fa-home w3-xxlarge"></i>
+    <p>accuiel</p>
+  </a>
+  <a href="#about" class="w3-bar-item w3-button w3-padding-large w3-hover-gris">
+    <i class="fa fa-user w3-xxlarge"></i>
+    <p>photos</p>
+  </a>
+  <a href="#contact" class="w3-bar-item w3-button w3-padding-large w3-hover-gris">
+    <i class="fa fa-envelope w3-xxlarge"></i>
+    <p>CONTACT</p>
+  </a>
+</nav>
+
+<!-- Navbar on small screens (Hidden on medium and large screens) -->
+<div class="w3-top w3-hide-large w3-hide-medium" id="myNavbar">
+  <div class="w3-bar w3-black w3-opacity w3-hover-opacity-off w3-center w3-small">
+    <a href="#" class="w3-bar-item w3-button" style="width:25% !important">accuiel</a>
+    <a href="#about" class="w3-bar-item w3-button" style="width:25% !important">a propos</a>
+    <a href="#photos" class="w3-bar-item w3-button" style="width:25% !important">PHOTOS</a>
+    <a href="#contact" class="w3-bar-item w3-button" style="width:25% !important">CONTACT</a>
+  </div>
+</div>
+
+  
+  
+
+<!-- Page Content -->
+<div class="w3-padding-large" id="main">
+<!-- Header/Home -->
+<header class="w3-container w3-padding-32 w3-center w3-black" id="home">
+  <h1 class="w3-jumbo"><span class="w3-hide-small" >chez le </span> delice.</h1>
+  <p>Les restaurants contemporains ne sont plus simplement des lieux pour se nourrir,
+    mais des espaces où l'art culinaire.</p>
+  <img src="f78f7495-96c1-4901-b4a0-e7aba5a7e9e3.jpeg" alt="boy" class="w3-image" width="700" height="1108">
+  <script language="JavaScript">
+    function verif() {
+        if (document.getElementById("nom").value == "") { alert("Veuillez taper votre nom!"); return false;}
+        if (document.getElementById("prenom").value == "") { alert("Veuillez taper votre prenom!"); return false;}
+
+     
+
+        if (document.getElementById("email").value == "") { alert("Veuillez entrer votre adresse e-mail!"); return false;}
+       if (document.getElementById("email").value.indexOf('@') == -1) { alert("Adresse e-mail incorrecte!"); return false; }
+       
+        
+alert("Inscription achevée") ;    
+    }
+</script>
+
+
+
+  	
+	
+
+
+
+
+      
+  <div id="about" class="a">
+    <h2 class="w3-border-bottom w3-border-light-grey w3-padding-16">a propos</h2>
+    <p>Bienvenue à chez le delice
+
+      Notre menu reflète notre engagement envers l'authenticité et
+       la créativité. Des plats classiques revisités aux créations uniques de notre chef, chaque bouchée vous transporte dans un voyage culinaire exquis. Explorez des saveurs 
+      inattendues, des combinaisons audacieuses et des présentations artistiques.
+    </p>
+    </div>
+    
+    <div class="w3-row-padding ">
+    
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="chop suey.jpeg" alt="John" style="width:100%">
+      <h3>chop suey</h3>
+      
+      <p>Chop Suey, où la fraîcheur des légumes croquants
+         se marie harmonieusement avec des morceaux juteux de poulet teriyaki. .</p></p>
+      <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+      </div>
+      
+      <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="sushi.jpeg" alt="Jane" style="width:100%">
+      <h3>sushi</h3>
+   
+      <p>
+            
+      Les sushis sont un plat japonais populaire composé de riz assaisonné vinaigré, associé à divers ingrédients tels que du poisson .</p>
+        <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+      </div>
+      
+      <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="Chow Fun (2).jpeg" alt="Mike" style="width:100%">
+      <h3>chow fun</h3>
+      
+      <p>Des larges nouilles de riz sautées à
+         la perfection dans un wok, accompagnées de fines lamelles de bœuf mariné,
+        </p>
+      <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+      </div>
+      
+      <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="soupe1.jpeg" alt="Dan" style="width:100%">
+      <h3>soupe wonton</h3>
+    
+      <p>  soupe wonton les  dumplings soigneusement confectionnés, 
+         remplis de porc haché et de crevettes  blancs fraîches, flottent  .</p></p>
+      <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+    </div>
+      
+  </div>
+  
+  <div id="about" class="w3-container w3-padding-32">
+   
+    
+    <div class="w3-row-padding ">
+    
+    <div class="w3-col l3 m6 w3-margin-bottom">
+    <img src="the.jpeg" alt="Dan" style="width:100%"> 
+  <h3> dragon well</h3>
+
+    <p>Des feuilles vertes plates produisent une infusion claire et légère avec des notes de noisette délicates..</p>
+    <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+    </div>
+    
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="Fried Rice.jpeg" alt="Jane" style="width:100%">
+      <h3>fried rice</h3>
+      
+      <p>Fried Rice exquis au poulet teriyaki. Des grains de riz sautés  
+      ,</p>
+      <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+      </div>
+      
+      <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="onigiri.jpeg" alt="Mike" style="width:120%">
+      <h3>onigiri</h3>
+      <p> Les onigiri sont souvent enveloppés dans une fine feuille d'algue nori, </p>
+      <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+      </div>
+      
+      <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="Tonjiru (Butajiru) Japanese Pork Miso Soup _ Sudachi.jpeg" alt="Dan" style="width:100%">
+      <h3>tonjuri</h3>
+      
+      <p> 
+       . Il est préparé en panant une tranche de porc dans de la chapelure panko</p>
+      <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+      </div>
+      
+      </div>
+      
+
+          
+          </div>
+          
+                
+      
+      
+  
+  
+    <!-- About Section -->
+    <div class="w3-content w3-justify w3-text-grey w3-padding-64" id="about">
+      <h2 class="w3-text-light-grey">notre creativite</h2>
+      <hr style="width:200px" class="w3-opacity">
+      <p>Les restaurants ne sont pas simplement des lieux où l'on satisfait
+         sa faim, mais des temples de l'art culinaire où chaque plat raconte une histoire. Que vous soyez à la recherche de saveurs traditionnelles, d'innovations gastronomiques ou d'une expérience culinaire exotique,
+         le monde des restaurants offre un voyage sensoriel unique.</p><br>
+        <p> Lorsque vous franchissez les portes d'un restaurant raffiné, vos sens sont immédiatement éveillés par l'arôme alléchant des plats en préparation et la vue d'une présentation artistique. Les chefs, tel des artistes,
+          transforment des ingrédients simples en œuvres d'art gastronomiques.</p>
+          <h3 class="w3-padding-16 w3-text-light-grey">notre art</h3>
+          <p class="w3-wide">cuisine</p>
+          <div class="w3-white">
+            <div class="w3-dark-grey" style="height:28px;width:95%"></div>
+          </div>
+          <p class="w3-wide">dance</p>
+          <div class="w3-white">
+            <div class="w3-dark-grey" style="height:28px;width:85%"></div>
+          </div>
+          
+      
+          <button class="w3-button w3-light-grey w3-padding-large w3-section">
+            <i class="fa fa-download"></i> Download Resume
+          </button>
+          <div class="w3-row-padding w3-margin-top">
+      
+            <div class="w3-third">
+            <div class="w3-card">
+              <img src="Ornina Arabian Lounge _ Al Bandar - Raha Beach _ Abu Dhabi (2).jpeg" style="width:100%">
+              <div class="w3-container">
+              <h4> tea</h4>
+              </div>
+              </div>
+              </div>
+              
+              <div class="w3-third">
+              <div class="w3-card">
+              <img src="1c230aa0-13fc-4233-84dd-452a5d3f1ee7.jpeg"  style="width:100%">
+              <div class="w3-container">
+              <h4>decor</h4>
+              </div>
+              </div>
+              </div>
+              
+              <div class="w3-third">
+                  <div class="w3-card">
+                  <img src="Was ein Duft! Papis Lamm Tajine mit Aprikosen - Tinastausendschön.jpeg"  style="width:100%">
+              
+              <div class="w3-container">
+              <h4>tajine</h4>
+              </div>
+            </div>
+          </div>
+          
+          </div>
+          <div class="w3-row-padding w3-margin-top">
+          
+          <div class="w3-third">
+          <div class="w3-card">
+          <img src="10746a46-f84c-4c6e-8120-7e2bf4b3351f.jpeg" style="width:100%">
+          <div class="w3-container">
+          <h4>gnawa</h4>
+          </div>
+          </div>
+          </div>
+          
+          <div class="w3-third">
+          <div class="w3-card">
+          <img src="Morocco (1).jpeg" style= "width:100%; right: 99%;">
+          <div class="w3-container">
+          <h4>sinniya</h4>
+          </div>
+          </div>
+          </div>
+          <div class="w3-third">
+            <div class="w3-card">
+            <img src="darna1.jpeg" style="width:100% ;right: 60;">
+            <div class="w3-container">
+            <h4>exhortation marocaine</h4>
+            </div>
+            </div>
+            </div>
+            
+            </div>
+              
+          <!-- Grid for pricing tables -->
+          
+      
+            
+               
+          <!-- End Grid/Pricing tables -->
+          </div>
+          
+          <!-- Testimonials -->
+          <h3 class="w3-padding-24 w3-text-light-grey"></h3> <div class="w3-padding-64 w3-content w3-text-grey" id="contact">
+          
+  
+  
+            <h2 class="w3-text-light-grey">Contactez nous</h2>
+            <hr style="width:200px" class="w3-opacity">
+        
+            <div class="w3-section">
+              <p><i class="fa fa-map-marker fa-fw w3-text-white w3-xxlarge w3-margin-right"></i> Casablanca</p>
+              <p><i class="fa fa-phone fa-fw w3-text-white w3-xxlarge w3-margin-right"></i> telephone:0600930078</p>
+              <p><i class="fa fa-envelope fa-fw w3-text-white w3-xxlarge w3-margin-right"> </i> Email: delice@gmail.com</p>
+            </div><br>
+            
+          
+
+            <form  name="f"   action="index.php"  method="post"   onSubmit="verif()">
+              <p><input  id="nom"   class="w3-input w3-padding-16" type="text" placeholder="nom" required name="nom"></p>
+              <p><input id="prenom" class="w3-input w3-padding-16" type="text" placeholder="prenom" required name="prenom"></p>
+              <p><input id="email" class="w3-input w3-padding-16" type="text" placeholder="Email" required name="Email"></p>
+              <p><input  id="sujet"  class="w3-input w3-padding-16" type="text" placeholder="Sujet" required name="Sujet"></p>
+              <p><input id="adresse" class="w3-input w3-padding-16" type="text" placeholder="adresse" required name="adresse"></p>
+            
+                <!--<button class="w3-button w3-light-grey w3-padding-large" type="submit">
+                  <i class="fa fa-paper-plane"></i> envoyer MESSAGE
+                </button> -->
+                <script>
+        function goToPage2() {
+            window.location.href="FORM.php" ;
+        }
+    </script>
+
+   
+    <button onclick="goToPage2()">login</button>
+
+                
+            </form>
+          <!-- End Contact Section -->
+          </div> 
+         
+            
+            
+          
+          <footer class="w3-content w3-padding-64 w3-text-grey w3-xlarge">
+            <i class="fa fa-facebook-official w3-hover-opacity"></i>
+            <i class="fa fa-instagram w3-hover-opacity"></i>
+            <i class="fa fa-snapchat w3-hover-opacity"></i>
+            <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+            <i class="fa fa-twitter w3-hover-opacity"></i>
+            <i class="fa fa-linkedin w3-hover-opacity"></i>
+            <p class="w3-medium">Powered by <a href="#" target="_blank" class="w3-hover-text-GREEN">NOUHAILA HALA</a></p>
+          <!-- End footer -->
+          </footer>
+      <!-- End photo grid -->
+  </div>
+  <!-- End Portfolio Section -->
+  </div>
+  
+  <!-- END PAGE CONTENT -->
+  </div>
+  
+  </body>
+  </html>
+        
+  
+  
+  <?php
+
+    try {
+        $conn = new PDO("mysql:host=localhost;dbname=restaurant;charset=utf8", 'root', '123456');
+    } catch(Exception $e) {
+        echo "Connection failed: " . $e->getMessage();
+        exit; // Quitte le script si la connexion échoue
+    }
+ if(isset($_POST['save']))   {
+$sql='insert into utilisateur(nom,prenom,Email,Sujet,adresse) values (:nom,:prenom,:gmail,:sujet,:adresse); ';
+$stm=$conn->prepare($sql);
+$stm->bindParam(":nom",$_POST['nom']);
+$stm->bindParam(":prenom",$_POST['prenom']);
+$stm->bindParam(":gmail",$_POST['Email']);
+$stm->bindParam(":sujet",$_POST['Sujet']);
+$stm->bindParam(":adresse",$_POST['adresse']);
+$stm->execute();}
+
+?>
+
+
+  
+  
+    <!-- Contact Section -->
+      
+
+
